341f1d369074c899a94dd11b4bb5f982e24dac49
StarCat <84149668+Star-Cat13@users.noreply.github.com>
2023-06-19 14:32:30 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://star-cat13.github.io/TS-Frenchies/'
+  base: 'https://star-cat13.github.io/Frenchies/'
 })
