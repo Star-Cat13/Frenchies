f5094961826c1ba171b862bc97b9883ad1c9b7da
StarCat <84149668+Star-Cat13@users.noreply.github.com>
2023-06-18 22:52:39 -0600

    tem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 // import MainLayout from './layout/MainLayout'
 import Home from './pages/homepage'
@@ -8,18 +8,18 @@ import Contact from './pages/contactpage'
 import Footer from './components/footer'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
 
     <div>
-
-      <Home />
-      <GridPix />
-      <About />
-      <Contact />
-      <Footer />
-
+      
+        <Home />
+        <GridPix />
+        <About />
+        <Contact />
+        <Footer />
+      
     </div>
   )
 }
