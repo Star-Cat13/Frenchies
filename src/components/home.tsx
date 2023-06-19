4f3e275e2fb9f93a1dd63417004ac52840b7f7fd
StarCat <84149668+Star-Cat13@users.noreply.github.com>
2023-06-19 14:41:42 -0600

    home

---
+++ src/components/home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <div>
+            <h1>Treasure State Frenchies</h1>
+            <div className="home">
+                <div className="workspace">
+                    <img src='/pictures/diamonddog.png' alt='diamond dog' />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
