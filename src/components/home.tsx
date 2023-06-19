7929a2fdb8f325cbb1445fdaf9d42d1c9e7bd4d9
StarCat <84149668+Star-Cat13@users.noreply.github.com>
2023-06-19 14:53:45 -0600

    Update home.tsx

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,10 @@
 function Home() {
     return (
         <div>
-            <h1>Treasure State Frenchies</h1>
+            <h1>Elemental Frenchies</h1>
             <div className="home">
                 <div className="workspace">
-                    <img src='./public/diamonddog.png' alt='diamond dog' />
+                    <img src='../public/diamonddog.png' alt='diamond dog' />
                 </div>
             </div>
         </div>
